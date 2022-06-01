27a4b25108210201d89311a97f003912fe8192fa
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-01 13:37:42 +0300

    Added ApplicationDbContext reference and User mgmt

--- Publico/Controllers/HomeController.cs
+++ Publico/Controllers/HomeController.cs
@@ -2,19 +2,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Publico.Models;
 
+using Publico.Models;
+using Publico.Data;
+
 namespace Publico.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
+    public readonly UserManager<AppUser> _userManager;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager, ApplicationDbContext context)
     {
         _logger = logger;
+        _userManager = userManager;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var currentUser = _userManager.GetUserAsync();
+        var messages = await _context.Messages.ToListAsync();
         return View();
     }
 
