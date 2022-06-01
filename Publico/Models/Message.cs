9d1db7750cafbebdab4f4c87f0da78a6132347b3
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-01 12:57:09 +0300

    Added a model for the chat elements

---
+++ Publico/Models/Message.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Publico.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    [Required]
+    public string? UserName { get; set; }
+    [Required]
+    public string? Text { get; set; }
+    public DateTime When { get; set; }
+}
