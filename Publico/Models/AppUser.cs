fae4527a98352b1080c607b42068f1d6bb0f3b60
KennedyMungai <kennedymungaifmab@gmail.com>
2022-06-01 13:01:59 +0300

    Added a foreign key that established 1 : many

--- Publico/Models/AppUser.cs
+++ Publico/Models/AppUser.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace Publico.Models;
 
 public class AppUser : IdentityUser
 {
-    
+    public AppUser()
+    {
+        Messages = new HashSet<Message>();
+    }
+
+    // 1 - * (AppUser to Messages)
+    public virtual ICollection<Message> Messages { get; set; }
 }
